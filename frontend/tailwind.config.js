a5aa1785a9b9ca2484072031aafb70f558e3a16f
Rushikesh karkhane <83975032+rushikeshkarkhane@users.noreply.github.com>
2024-12-23 21:16:40 +0530

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,25 +1,23 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  darkMode:'selector',
+// tailwind.config.js
+module.exports = {
+  darkMode: 'class',  
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
     "./node_modules/flowbite/**/*.js"
-    
   ],
   theme: {
     extend: {
       colors: {
         slate: {
-          750:'#172231',
-          850:'#263448',
-          150:'#f5f7f9'
+          750: '#172231',
+          850: '#263448',
+          150: '#f5f7f9'
         },
       },
     },
   },
- 
   plugins: [
-        require('flowbite/plugin')
+    require('flowbite/plugin')
   ],
 }
